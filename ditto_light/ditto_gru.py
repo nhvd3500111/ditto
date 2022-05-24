9280d184d8ddaa7d0d14c650b68b5af1b5d7baec
Konstantinos Panoutsakos <71216559+nhvd3500111@users.noreply.github.com>
2022-05-24 22:53:08 +0300

    Add files via upload

--- ditto_light/ditto_gru.py
+++ ditto_light/ditto_gru.py
@@ -89,8 +89,8 @@ class DittoModel(nn.Module):
         enc=torch.reshape(enc, (enc.shape[0],1, enc.shape[1])) #We want it to match with the 3-d array input of the gru- so we put 
         #batch first , one then (since we take only the CLS token) and then the length of the representation of the bert model
         
-
-        return self.fc(enc) # .squeeze() # .sigmoid()
+        enc=enc.type(torch.float())
+        reeturn self.fc(enc) # .squeeze() # .sigmoid()
 
 
 def evaluate(model, iterator, threshold=None):
