efe9f4058679a20255ce577507fe4f4d68957833
Konstantinos Panoutsakos <71216559+nhvd3500111@users.noreply.github.com>
2022-06-14 19:33:26 +0300

    Update dataset.py

--- ditto_light/dataset.py
+++ ditto_light/dataset.py
@@ -72,7 +72,7 @@ class DittoDataset(data.Dataset):
         x = self.tokenizer.encode(text=left,
                                   text_pair=right,
                                   max_length=self.max_len,
-                                  truncation=True,padding=True)
+                                  truncation=True)
 
         # augment if da is set
         if self.da is not None:
@@ -81,7 +81,7 @@ class DittoDataset(data.Dataset):
             x_aug = self.tokenizer.encode(text=left,
                                       text_pair=right,
                                       max_length=self.max_len,
-                                      truncation=True,padding=True)
+                                      truncation=True)
             return x, x_aug, self.labels[idx]
         else:
             return x, self.labels[idx]
